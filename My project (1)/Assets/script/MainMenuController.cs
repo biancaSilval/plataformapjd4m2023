9c4f3fad483957e0c88c3eff783711b53708891c
Bianca Silval <biancasilvagomes2004@gmail.com>
2023-04-12 10:36:37 -0300

    commit

---
+++ My project (1)/Assets/script/MainMenuController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LoadLevel(string LevelName)
+    {
+        GameManager.Instance.LoadScene("LevelName");
+    }
+
+    
+}
